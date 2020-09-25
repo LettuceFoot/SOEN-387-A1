8ea8981908260e254245a91470e477a8dfc5aa14
Ramy Elhoufy <pandavader55@gmail.com>
2020-09-25 16:29:02 -0400

    initial commit

---
+++ src/main/java/ChatServlet.java
@@ -0,0 +1,31 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ChatServlet")
+public class ChatServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ChatApp</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ChatApp at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (IOException exception){
+            exception.getMessage();
+        }
+
+    }
+}
